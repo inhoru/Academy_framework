dbce53d78c604eda228c158416f9ccee21c499e5
Inho Choi <inhoru126@gmail.com>
2023-07-13 23:09:23 +0900

    Delete spring directory

--- spring/src/main/java/com/bs/spring/Test.java
+++
@@ -1,9 +0,0 @@
-package com.bs.spring;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Test {
-	
-
-}
--- spring/src/main/java/com/bs/spring/include/TargetComponent.java
+++
@@ -1,8 +0,0 @@
-package com.bs.spring.include;
-
-import lombok.Data;
-
-@Data
-public class TargetComponent {
-	private String title="targetComponent";
-}
